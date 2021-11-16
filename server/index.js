3fe06d4948e878e5f6a8c1360d92ec2714df3634
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-16 15:57:53 +0100

    router been added

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-const PORT = process.env.Port || 5000;
-
+const PORT = process.env.Port || 5001;
+const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -11,6 +11,6 @@ const io = socketio(server);
 
 
 
-
+app.use(router);
 
 server.listen(PORT, ()=> console.log(`servers working on port ${PORT}`));
