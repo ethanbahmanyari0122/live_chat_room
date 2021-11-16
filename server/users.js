9e257be62b2c48fb2aeb1e3d52d2c1bcbfacb154
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-16 19:53:47 +0100

    users functions helper added

--- server/users.js
+++ server/users.js
@@ -1,15 +1,29 @@
 const users = [];
 
 const addUser = ({id, name, room})=>{
+    name = name.trim().toLocaleLowerCase();
+    room = room.trim().toLocaleLowerCase();
 
+    const existingUser = users.find((user)=> user.room === room && user.name === name);
+
+    if(existingUser){
+        return {error: 'Username is taken'};
+    }
+
+    const user = {id, name, room};
+    users.push(user);
+    return {user};
 }
 
-const removeUser = ()=>{
-
+const removeUser = (id)=>{
+    const index = users.findIndex((user)=> user.id === id);
+    if(index !== -1){
+        return users.splice(index,1)[0];
+    }
 }
 
-const getUser = ()=>{
-
+const getUser = (id)=>{
+    users.find((user)=> user.id === id);
 }
 
 const getUsersInRoom = ()=>{
