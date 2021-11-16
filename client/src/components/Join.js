5010f5a00532797fe6df961a6be54ecf939cab69
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-16 17:52:10 +0100

    inputs and heading has been added

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const Join = ()=> {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
     return(
-        <h1>Join</h1>
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="Name" className="joinInput" type="text" onChange={} /></div>
+                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={} /></div>
+                <Link>
+                    <button className="button mt-20" type="submit" >Sign In</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
