436b8724ddfab07264b2e290dcfb5315f5aa87e6
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-16 15:49:39 +0100

    s

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Join from "./components/Join";
+import Chat from "./components/Chat";
+
+
+const App = ()=>{
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={<Join/>}/>
+                <Route path="/chat" element={<Chat/>}/>
+            </Routes>
+        </Router>
+    )
+}
+
+export default App;
