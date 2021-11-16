61166d98f41e97d85e49b291dd2f712a30a944e6
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-16 18:10:58 +0100

    first hook has been added to the chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {  useState, useEffect } from "react";
+import querystring from "querystring";
+import io from "socket.io-client";
 
 
 const Chat = ()=> {
+    useEffect(()=>{
+        const data = querystring.parse(location.search)
+    })
+
     return(
         <h1>Chat</h1>
     )
