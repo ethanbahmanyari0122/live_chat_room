c234950038041a2dfba75048ff40b9392e74410c
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-17 09:28:21 +0100

    admin welcome message has been added

--- server/index.js
+++ server/index.js
@@ -26,7 +26,16 @@ io.on('connection', (socket)=>{
         socket.broadcast.to(user.room.emit('message', {user:'admin', text: `${user.name}, has joined!`}));
 
         socket.join(user.room);
+
+        callback();
     });
+    socket.on('sendMessage', (message, callback)=> {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message});
+        callback();
+    });
+
     socket.on('disconnect', ()=>{
         console.log('User had left!');
     });
--- server/users.js
+++ server/users.js
@@ -23,7 +23,7 @@ const removeUser = (id)=>{
 }
 
 const getUser = (id)=>{
-    users.find((user)=> user.id === id);
+    return users.find((user)=> user.id === id);
 }
 
 const getUsersInRoom = (room)=>{
