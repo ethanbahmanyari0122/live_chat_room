0794974eeb182c6e3e04e5186488c7a7a5904695
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-17 11:12:48 +0100

    admin welcome message has been added

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -11,13 +11,20 @@ const Message = ({message: {user, text },name}) => {
     }
     return (
         isSentByCurrentUser ? (
-            <div className="messageContainer">
-                <p className="sentText">{trimmedName}</p>
-                <div className="messageBox">
-                    <p className="messageText">{text}</p>
+            <div className="messageContainer justifyEnd">
+                <p className="sentText pr-10">{trimmedName}</p>
+                <div className="messageBox backgroundBlue">
+                    <p className="messageText colorWhite">{text}</p>
                 </div>
             </div>
-        ) : ()
+        ) : (
+            <div className="messageContainer justifyStart">
+                <div className="messageBox backgroundLight">
+                    <p className="messageText colorDark">{text}</p>
+                </div>
+                <p className="sentText pl-10">{user}</p>
+            </div>
+        )
 
     )}
 
--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Messages.css";
-import ScrollToBottm from 'react-scroll-to-bottom';
+import ScrollToBottom from 'react-scroll-to-bottom';
 import Message from "../Message/Message"
 
-const Messages = (messages, name) => {
+
+const Messages = ({messages, name}) => {
     return (
-        <ScrollToBottm>
+        <ScrollToBottom className="messages">
             {messages.map((message , i)=> <div key={i}><Message message={message} name={name}/></div> )}
-        </ScrollToBottm>
+        </ScrollToBottom>
     )}
 
 export default Messages;
