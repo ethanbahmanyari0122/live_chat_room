4f8643ea543e85680f1a18278e2a0b8c36186355
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-17 09:55:02 +0100

    admin welcome message has been added

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,9 +7,8 @@ const Chat = ({ location })=> {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
     const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState('');
+    const [messages, setMessages] = useState([]);
 
-    const ENDPOINT = 'localhost:5001';
     useEffect(()=>{
         const {name, room} = queryString.parse(location.search);
 
@@ -17,16 +16,18 @@ const Chat = ({ location })=> {
         setName(name);
         setRoom(room);
 
-        socket.emit('join', { name , room }, ()=>{
-
+        socket.emit('join', { name , room }, (error)=>{
+            if(error){
+                alert(error);
+            }
         });
 
         return () => {
-            socket.emit('disconnect');
+            socket.disconnect();
 
             socket.off();
         }
-    })
+    },)
 
     useEffect(()=>{
         socket.on('message', (message)=>{
@@ -41,7 +42,9 @@ const Chat = ({ location })=> {
         if(message) {
             socket.emit('sendMessage', message, ()=> setMessage(''));
         }
+        setMessage('')
     }
+    console.log(message, messages);
     return(
         <div className="outerContainer">
             <div className="container">
