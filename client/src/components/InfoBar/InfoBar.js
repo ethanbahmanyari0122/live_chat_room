0e9512c94e656af2052fc759a2ca1e459f5f49da
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-17 10:24:32 +0100

    admin welcome message has been added

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./InfoBar.css";
+import ecology from "../../icons/ecology.png";
+import close from "../../icons/close.png";
 
 const InfoBar =() => {
     <div className="infoBar">
         <div className="leftInnerContainer">
-            <img className="onlineIcon" src={onlineIcon}/>
+            <img className="onlineIcon" src={ecology}/>
             <h3>roomName</h3>
         </div>
         <div className="rightInnerContainer">
-            <a href="/"><img src={closeIcon} alt={"close image"}/></a>
+            <a href="/"><img src={close} alt={"close image"}/></a>
         </div>
     </div>
 }
