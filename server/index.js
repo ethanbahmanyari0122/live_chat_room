65e09536ba59f58c29e171ac9f69648a10fc8a65
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-17 11:36:15 +0100

    remove user logic has been applied

--- server/index.js
+++ server/index.js
@@ -43,7 +43,11 @@ io.on('connection', (socket)=>{
     });
 
     socket.on('disconnect', ()=>{
-        console.log('User had left!');
+        const user = removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('message', {user:'admin', text:`${user.name} has left.`})
+        }
     });
 })
 
